ebaeb50c6b9c22c73bee76d80aa7cfe39779f386
MatthewZhaohaiHu <Matthew.zhaohai.hu@gmail.com>
2017-09-15 23:33:08 -0400

    Update UI and handleButtonAction(CalculatorController.java)

--- src/application/CalculatorController.java
+++ src/application/CalculatorController.java
@@ -1,5 +1,87 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class CalculatorController {
-	
+
+    @FXML
+    private Button sin;
+
+    @FXML
+    private Button leftbrackets;
+
+    @FXML
+    private Button seven;
+
+    @FXML
+    private Button four;
+
+    @FXML
+    private Button one;
+
+    @FXML
+    private Button eight;
+
+    @FXML
+    private Button five;
+
+    @FXML
+    private Button cos;
+
+    @FXML
+    private Button two;
+
+    @FXML
+    private Button nine;
+
+    @FXML
+    private Button dot;
+
+    @FXML
+    private Button six;
+
+    @FXML
+    private Button undo;
+
+    @FXML
+    private Button rightbrackets;
+
+    @FXML
+    private Button powerof;
+
+    @FXML
+    private Button allcrear;
+
+    @FXML
+    private Button delete;
+
+    @FXML
+    private Button devision;
+
+    @FXML
+    private Button three;
+
+    @FXML
+    private Button equal;
+
+    @FXML
+    private Button times;
+
+    @FXML
+    private Button plus;
+
+    @FXML
+    private Button minus;
+
+    @FXML
+    private TextField display;
+
+    @FXML
+    void handleButtonAction(ActionEvent event) {
+    		System.out.println("yeah?");
+    }
+
 }
